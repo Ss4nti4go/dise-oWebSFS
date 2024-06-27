f4f8a42e8e0b6100944cb22edb2740c734ee47be
Ss4nti4go <153297179+Ss4nti4go@users.noreply.github.com>
2024-06-26 22:48:50 -0300

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,14 @@
-function expandCard(card) {
-    card.classList.toggle('expanded');
-    document.body.classList.toggle('noscroll');
+const overlay = document.getElementById('overlay');
+const card = document.getElementById('section-categorias');
+function expandCard(element) {
+    element.classList.add('expanded');
+    document.querySelector('.overlay').style.display = 'block';
+    document.querySelector('.cerrar-card').style.display = 'block';
+}
+function cerrarCardOverlay() {
+    document.querySelector('.section-categorias.expanded').classList.remove('expanded');
+    document.querySelector('.cerrar-card').style.display = 'none';
+    document.querySelector('.overlay').style.display = 'none';
 }
 function toggleForm(formType) {
     const formContainer = document.getElementById('form-container');
@@ -8,19 +16,21 @@ function toggleForm(formType) {
     const registerForm = document.getElementById('register-form');
     const loginBtn = document.getElementById('login-btn');
     const registerBtn = document.getElementById('register-btn');
-    
-    if (formType === 'login') {
+   
+    if(formType === 'login') {
         loginForm.classList.add('active', 'moverse-dentro-izquierda');
-        registerForm.classList.remove('active', 'moverse-dentro-derecha');
+        registerForm.classList.remove('active', 'moverse-fuera-derecha');
         loginForm.classList.remove('inactive');
-        registerForm.classList.add('inactive', 'moverse-fuera-derecha');
+        registerForm.classList.add('inactive', 'moverse-dentro-derecha');
         loginBtn.style.backgroundColor = '#555';
         registerBtn.style.backgroundColor = '#333';
+        
     } else if (formType === 'register') {
+      
         registerForm.classList.add('active', 'moverse-dentro-derecha');
-        loginForm.classList.remove('active', 'moverse-dentro-izquierda');
+        loginForm.classList.remove('active', 'moverse-fuera-izquierda');
         registerForm.classList.remove('inactive');
-        loginForm.classList.add('inactive', 'moverse-fuera-izquierda');
+        loginForm.classList.add('inactive', 'moverse-dentro-izquierda');
         registerBtn.style.backgroundColor = '#555';
         loginBtn.style.backgroundColor = '#333';
     }
